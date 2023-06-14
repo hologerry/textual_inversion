3884761e320557335bbd5e0194f218ab6fd1198a
Yue Gao <hologerry@gmail.com>
2023-06-14 17:43:37 +0800

    update

--- invert_bottle.sh
+++ invert_bottle.sh
@@ -1,7 +1,8 @@
 python main.py --base configs/latent-diffusion/txt2img-1p4B-finetune.yaml \
-               -t \
-               --actual_resume models/ldm/text2img-large/model.ckpt \
-               -n invert_bottle \
-               --gpus 0, \
-               --data_root ../data/water_bottle_single_cat/nongfu_img_pad_bottle \
-               --init_word "bottle" \
+    -t \
+    --actual_resume models/ldm/text2img-large/model.ckpt \
+    -n invert_bottle \
+    --gpus 0, \
+    --data_root ../data/water_bottle_single_cat/nongfu_img_pad_bottle \
+    --init_word "bottle" \
+    --no-test \
